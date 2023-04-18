08d492a3aa814ae1c9bb51a2d93ec4be6d501343
Julien Bolle <julien.bolle@clickandboat.com>
2023-04-18 14:20:58 +0200

    test signal with compute, mutate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal, computed } from '@angular/core';
+import { PokemonService } from './pokemon.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'poc-signal';
+export class AppComponent implements OnInit {
+  user = signal({
+    name: 'Bolle',
+    surname: 'Julien',
+    isConnected: false
+  });
+
+  fullName = computed(() => {
+    return `${this.user().surname} ${this.user().name}` 
+  }); 
+
+  constructor(private pokemonService: PokemonService) { }
+
+  ngOnInit(): void {
+    // this.pokemonService.getAll().subscribe(x => {
+    //   console.log(x);
+    // })
+  }
+
+  connect() {
+    this.user.mutate(u => {
+      u.isConnected = true;
+    });
+  }
 }
